0a190d4c905fc158265defae417ad62866426f00
ahmad farhan <i140099@nu.edu.pk>
2017-03-30 13:50:00 +0500

    work on Products (1)

---
+++ MVC/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult AddProduct()
+        {
+            return View();
+        }
+        public ActionResult DeleteProduct()
+        {
+            return View();
+        }
+    }
+}
