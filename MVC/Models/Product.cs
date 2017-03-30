23442be429c9b2b0c7c7ee02d34014a9386f2e01
ahmadfarhan <i140099@nu.edu.pk>
2017-03-30 21:31:21 +0500

    product 4

--- MVC/Models/Product.cs
+++ MVC/Models/Product.cs
@@ -8,15 +8,13 @@ namespace MVC.Models
     public class Product
     {
         
-        public string ID { set; get; }
 
-        public string ProductName { set; get; }
-
+        public string Name { set; get; }
+        public int Quantity{ set; get; }
         
-        public string DefaultPrice { set; get; }
-
         public string Price { set; get; }
 
+        public string Description{ set; get; }
         public byte[] Image { get; set; }
 
     }
