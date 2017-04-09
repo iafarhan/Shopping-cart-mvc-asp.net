d71eefa545c651e895211a2b89e8b375c365b067
ahmadfarhan <i140099@nu.edu.pk>
2017-04-09 17:50:15 +0500

    products with sql CRUD backend done(1)

--- MVC/Global.asax.cs
+++ MVC/Global.asax.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,6 +13,7 @@ namespace MVC
     {
         protected void Application_Start()
         {
+           // Database.SetInitializer<ProductDBContext>(null);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
--- MVC/Models/Product.cs
+++ MVC/Models/Product.cs
@@ -1,21 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
 
-namespace MVC.Models
+public partial class Product
 {
-    public class Product
-    {
-        
+    public string Name { get; set; }
+    public int Quantity { get; set; }
+    public int Price { get; set; }
+    public string Description { get; set; }
+    public byte[] ProductImage { get; set; }
+    public int id { get; set; }
+}
 
-        public string Name { set; get; }
-        public string Quantity{ set; get; }
-        
-        public string Price { set; get; }
-
-        public string Description{ set; get; }
-        public byte[] Image { get; set; }
-
-    }
+public class ProductDBContext : DbContext
+{
+    public DbSet<Product> products { get; set; }
 }
